094a7218f5dbe8c6ca7760e1144efd331d140b59
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2026-03-19 16:30:15 +0100

    feat: Add Nitro integration to Vite configuration and dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
@@ -19,6 +20,7 @@ const config = defineConfig({
         generatedRouteTree: './routeTree.gen.ts',
       },
     }),
+    nitro(),
     viteReact(),
   ],
 })
